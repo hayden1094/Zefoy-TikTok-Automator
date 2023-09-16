c12ad2024e9d91760113ac013fd002c2d5b9733c
hayden1094 <128063625+hayden1094@users.noreply.github.com>
2023-09-16 17:36:43 +0100

    Update Install Requirements.bat

--- Install Requirements.bat
+++ Install Requirements.bat
@@ -1,5 +1,5 @@
 pip install undetected-chromedriver==3.1.7
-pip install colorama==0.4.6
-pip install selenium==4.7.2
+pip install colorama==0.4.4
+pip install selenium==4.1.3
 pip install requests==2.27.1
 pause
